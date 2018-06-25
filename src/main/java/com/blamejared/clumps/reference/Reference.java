54c6692c670dbf840bf3829473d46ca1cf951559
Jared <jaredlll08@gmail.com>
2018-06-25 03:27:33 +0200

    version push

--- src/main/java/com/blamejared/clumps/reference/Reference.java
+++ src/main/java/com/blamejared/clumps/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "clumps";
 	public static final String NAME = "Clumps";
-    public static final String VERSION = "3.1.0";
+    public static final String VERSION = "3.1.1";
     public static final String DEPENDECIES = "";
 	
 	
