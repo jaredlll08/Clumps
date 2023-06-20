efb3d1021cf0738a3ca27ff1254e34ac9fd7ff12
Jared <jaredlll08@gmail.com>
2023-06-20 11:04:49 +0200

    Port to 1.20.1
    Close #123

--- buildSrc/src/main/kotlin/com/blamejared/clumps/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/clumps/gradle/Versions.kt
@@ -1,10 +1,10 @@
 package com.blamejared.clumps.gradle
 
 object Versions {
-    const val MOD = "11.0.0"
+    const val MOD = "12.0.0"
     const val JAVA = "17"
-    const val MINECRAFT = "1.20"
-    const val FORGE = "46.0.14"
+    const val MINECRAFT = "1.20.1"
+    const val FORGE = "47.0.4"
     const val FABRIC_LOADER = "0.14.21"
-    const val FABRIC = "0.83.0+1.20"
+    const val FABRIC = "0.83.1+1.20.1"
 }
